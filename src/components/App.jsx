1a40215e633a5441b17859fdc4f70976d79329d5
Majician13 <44847744+Majician13@users.noreply.github.com>
2022-05-03 16:30:10 -0400

    Changed renderConditionally() function to ternary operator in App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,16 +3,23 @@ import Login from "./Login";
 
 var isLoggedIn = false;
 
-function renderConditionally() {
-  if (isLoggedIn) {
-    return <h1>Hello</h1>;
-  } else {
-    return <Login />;
-  }
-}
+// function renderConditionally() {
+//   if (isLoggedIn) {
+//     return <h1>Hello</h1>;
+//   } else {
+//     return <Login />;
+//   }
+// }
 
 function App() {
-  return <div className="container">{renderConditionally()}</div>;
+  return (
+    <div className="container">
+      {
+        //BELOW TERNARY LINE DOES SAME AS ABOVE COMMENTED OUT FUNTION RENDERCONDITIONALLY().
+        isLoggedIn ? <h1>Hello</h1> : <Login />
+      }
+    </div>
+  );
 }
 
 export default App;
